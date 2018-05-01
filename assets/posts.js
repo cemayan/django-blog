92f11378c25be3d04b05f2ae662bdd40bb4ecbc4
Cem <cem_ayan@outlook.com>
2018-05-02 02:24:24 +0300

    ..

--- assets/posts.js
+++ assets/posts.js
@@ -1,34 +1,37 @@
 
-var vue = new Vue({
-  el : '#posts',
-  data: {
-    posts :null
-  },
-  created: function () {
-    this.fetchData()
-  },
-  methods: {
-    fetchData : function(){
-      var self= this  
-      fetch('/api',{
-          method: "get",
-          credentials: "same-origin",
-          headers: {
-              "X-CSRFToken": getCookie("csrftoken"),
-              "Accept": "application/json",
-              "Content-Type": "application/json"
-          }, 
-        }).then(x=>x.json()).then(function(data){
-          if(typeof(data)==="string"){
-            var arr=[]
-            JSON.parse(data).some(function(obj){
-                arr.push(JSON.stringify(obj))
-            })
-            self.posts = arr
-          }else{
-            self.posts = data
-          }
-        })
+if(document.location.pathname =="/"){
+  var vue = new Vue({
+    el : '#posts',
+    data: {
+      posts :null
     },
-  }
-})
+    created: function () {
+      this.fetchData()
+    },
+    methods: {
+      fetchData : function(){
+        var self= this  
+        fetch('/api',{
+            method: "get",
+            credentials: "same-origin",
+            headers: {
+                "X-CSRFToken": getCookie("csrftoken"),
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            }, 
+          }).then(x=>x.json()).then(function(data){
+            if(typeof(data)==="string"){
+              var arr=[]
+              JSON.parse(data).some(function(obj){
+                  arr.push(JSON.stringify(obj))
+              })
+              self.posts = arr
+            }else{
+              self.posts = data
+            }
+          })
+      },
+    }
+  })
+  
+}
